8dbf13c452e556fcedce9d912bd9f6f87fa3b055
Sai Aditya Cheruku <saiadityacheruku@gmail.com>
2025-07-10 16:42:59 +0000

    increase the in app notifaction time by 20 ms

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,6 +1,6 @@
 
 
-export const THINKING_OF_YOU_DURATION = 30 * 1000; // 30 seconds for testing, original 10 * 60 * 1000 for 10 mins
+export const THINKING_OF_YOU_DURATION = 30 * 1000 + 20; // 30 seconds and 20ms for testing, original 10 * 60 * 1000 for 10 mins
 export const MAX_AVATAR_SIZE_KB = 500;
 
 /**
